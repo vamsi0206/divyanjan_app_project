0707062312c5cbb0be13fce777ca6f1fddf63c60
Raheel Ali <f20230305@hyderabad.bits-pilani.ac.in>
2025-06-11 15:02:04 +0530

    messing around

--- learning_node.js
+++ learning_node.js
@@ -2,14 +2,18 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  user: "hellojee",
+  password: "Cris1234@"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  connection.query("CREATE DATABASE mydb", function (err, result) {
+    connection.query("use mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+   connection.query("create table userInfo(name varchar(30), age int)", function (err, result) {
     if (err) throw err;
     console.log("Database created");
   });
